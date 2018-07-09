2240e94e542a4dc5885aeff53e2330690f01f551
Shobhit Chittora <chittorashobhit@gmail.com>
2018-07-09 21:38:00 +0530

    grpc server and client

---
+++ src/grpc-client.js
@@ -0,0 +1,22 @@
+const grpc = require('grpc');
+const loader = require('@grpc/proto-loader');
+
+loader.load('greet.proto', { includeDirs: ['./src', './'] }).then((packageDefinition) => {
+  const { grettings_package } = grpc.loadPackageDefinition(packageDefinition);
+  const Client = grettings_package.Greeting;
+  const client = new Client('0.0.0.0:8080', grpc.credentials.createInsecure());
+
+  client.greetDefault({}, function (err, res) {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log(res);
+  });
+
+  client.greetName({ name: 'Shobhit' }, function (err, res) {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log(res);
+  });
+});
--- src/grpc-server.js
+++ src/grpc-server.js
@@ -1,6 +1,29 @@
 const grpc = require('grpc');
 const loader = require('@grpc/proto-loader');
 
-loader.load('greet', () => {
+const HOST = `0.0.0.0:${process.env.PORT || 3000}`;
 
-});
+class GreetingsHandler {
+  greetDefault(_, callback) {
+    return callback(null, { message: 'Hello world!' });
+  }
+
+  greetName(call, callback) {
+    return callback(null, { message: `Hello ${call.request.name}!` });
+  }
+}
+
+const createServer = function (host, handler) {
+  loader.load('greet.proto', { includeDirs: ['./src', './'] }).then((packageDefinition) => {
+    const { grettings_package } = grpc.loadPackageDefinition(packageDefinition);
+    const service = grettings_package.Greeting.service;
+    const server = new grpc.Server();
+    server.addService(service, handler);
+    server.bind(host, grpc.ServerCredentials.createInsecure());
+    server.start();
+  });
+}
+
+createServer(HOST, new GreetingsHandler);
+
+console.log(`Server running on ${HOST}`);
