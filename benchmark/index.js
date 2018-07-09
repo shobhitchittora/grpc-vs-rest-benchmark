663ceffcb9ad0af3b12f0b642f8f59a55df6849c
Shobhit Chittora <chittorashobhit@gmail.com>
2018-07-09 22:27:23 +0530

    updates for /greet post request benchmarking

--- benchmark/index.js
+++ benchmark/index.js
@@ -5,8 +5,9 @@ const suite = new Benchmark.Suite;
 
 suite
   .add('GRPC', { defer: true, fn: grpcGreet })
+  .add('GRPC#NAME', { defer: true, fn: grpcGreetName })
   .add('REST', { defer: true, fn: restGreet })
-
+  .add('REST#NAME', { defer: true, fn: restGreetName })
   .on('cycle', function (event) {
     console.log(String(event.target));
   })
@@ -25,9 +26,47 @@ function restGreet(deferred) {
   }).on('error', console.log);
 }
 
+function restGreetName(deferred) {
+  const postData = JSON.stringify({ name: 'Shobhit' });
+
+  const options = {
+    hostname: 'localhost',
+    port: 3000,
+    path: '/greet',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData)
+    }
+  };
+
+  const req = http.request(options, (res) => {
+    let rawData;
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      deferred.resolve()
+    });
+  });
+
+  req.on('error', console.log);
+  req.write(postData);
+  req.end();
+}
+
 function grpcGreet(deferred) {
   grpcClient().then((client) => {
     client.greetDefault({}, function (err, res) {
+      if (err) {
+        return console.log(err);
+      }
+      return deferred.resolve();
+    });
+  });
+}
+
+function grpcGreetName(deferred) {
+  grpcClient().then((client) => {
+    client.greetDefault({ name: 'Shobhit' }, function (err, res) {
       if (err) {
         return console.log(err);
       }
--- src/rest-server.js
+++ src/rest-server.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('PORT', process.env.PORT || 8080);
 
 app.get('/', (_, res) => {
   res.json({ message: 'Hello world' });
 });
 
-app.get('/greet', (req, res) => {
-  res.json({ message: `Hello ${req.query.name || 'Master'}!` });
+app.post('/greet', (req, res) => {
+  res.json({ message: `Hello ${req.body.name || 'Master'}!` });
 });
 
 app.listen(app.get('PORT'), () => console.log(`Rest server running on ${app.get('PORT')}`));
